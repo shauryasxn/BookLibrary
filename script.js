c24fb6160e198eaf5c951f4ce141f9f1a896ff2e
shauryasxn <shaurya1003@gmail.com>
2023-10-13 01:23:59 +0530

    Initial Commit from local repo

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("feedback-button").addEventListener("click", function() {
+    document.getElementById("feedback-popup").style.display = "block";
+});
+
+document.getElementById("feedback-close").addEventListener("click", function() {
+    document.getElementById("feedback-popup").style.display = "none";
+});
